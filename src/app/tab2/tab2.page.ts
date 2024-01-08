ef8c50666653845accfe32ef8f35412f41392577
Roy Go Raposon Jr <roy@jarvisanalytics.com>
2024-01-09 02:38:39 +0800

    added PWA

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment'; 
+
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public sowingDate = '';
+  public transplantingDate = '';
+  public harvestDate = '';
+  public disableSaveButton = true;
+  entries: any[] = [];
+  
 
-  constructor() {}
+  constructor() {
+    const entries = localStorage.getItem('lettuceEntries');
+    if (entries) {
+      this.entries = JSON.parse(entries);
+    }
+  }
+
+  ngOnInit() {
+    // Load entries from localStorage on page load
+    const storedEntries = localStorage.getItem('lettuceEntries');
+    if (storedEntries) {
+      this.entries = JSON.parse(storedEntries);
+    }
+  }
+
+  calculateDates() {
+    const selectedDate = moment(this.sowingDate);
+    this.transplantingDate = selectedDate.add(15, 'days').format('YYYY-MM-DD');
+    this.harvestDate = selectedDate.add(30, 'days').format('YYYY-MM-DD');
+    this.disableSaveButton = false;
+  }
+
+  saveEntry() {
+    const entry = {
+      sowingDate: this.sowingDate,
+      transplantingDate: this.transplantingDate,
+      harvestDate: this.harvestDate,
+    };
+    this.entries.push(entry);
+    localStorage.setItem('lettuceEntries', JSON.stringify(this.entries));
+    this.resetFields();
+  }
+
+  deleteEntry(index: number) {
+    this.entries.splice(index, 1);
+    localStorage.setItem('lettuceEntries', JSON.stringify(this.entries));
+  }
+
+  resetFields() {
+    this.sowingDate = '';
+    this.transplantingDate = '';
+    this.harvestDate = '';
+    this.disableSaveButton = true;
+  }
 
 }
